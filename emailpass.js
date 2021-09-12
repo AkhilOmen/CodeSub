ce5444dd567480c30c30670c6bf57c3fa030c669
AkhilOmen <88222793+AkhilOmen@users.noreply.github.com>
2021-09-12 10:59:03 +0530

    Update emailpass.js

--- emailpass.js
+++ emailpass.js
@@ -1,11 +1,11 @@
 module.exports = {
     
     Emial:[
-        `nepeh14254@enpaypal.com`
+        `Enter mail.........`
     ],
 
     Password:[
-        `SuMa.g@4vY3QXv^`
+        `Enter password........`
     ]
 
 }
